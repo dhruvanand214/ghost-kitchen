e535f7ec98ec4f756697d1ccc760a244f48e1908
Dhruv Anand <71182136+dhruvanand214@users.noreply.github.com>
2026-02-14 01:46:31 +0530

    db change

--- apps/backend/src/config/db.ts
+++ apps/backend/src/config/db.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+const dns = require('node:dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Forces Node to use Google DNS
+
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI as string);
